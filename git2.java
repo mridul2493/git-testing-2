5cbb289cf2f57aa63a6e9b6390b388dfa834d99f
Mridul Yadav <122670350+mridul2493@users.noreply.github.com>
2025-02-21 20:45:45 +0530

    Cloning git2.java to local machine

---
+++ git2.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class git2{
+  public static void main(String [] args){
+    Scanner sc = new Scanner(System.in);
+    int m = sc.nextInt();
+    System.out.println("This is m = "+m);
+  }
+}
